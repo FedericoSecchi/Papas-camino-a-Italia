31fdb64405efcdebf64c4960dd84447a0acf74b4
FedericoSecchi <federico.secchimarino@gmail.com>
2023-03-24 12:36:35 -0400

    papas viaje a italia

---
+++ java.js
@@ -0,0 +1,18 @@
+var countdownDate = new Date("APRIL 3, 2023 18:45:59").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countdownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
